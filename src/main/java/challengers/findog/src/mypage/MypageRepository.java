6659910293fd70557a295c5c501a501d5e8121eb
gahyeonn <parka0017@gmail.com>
2022-05-15 15:09:19 +0900

    [delete] 내 정보 수정 기능 기존 코드 삭제

--- src/main/java/challengers/findog/src/mypage/MypageRepository.java
+++ src/main/java/challengers/findog/src/mypage/MypageRepository.java
@@ -1,7 +1,5 @@
 package challengers.findog.src.mypage;
 
-import challengers.findog.src.mypage.model.PatchUserInfoReq;
-import challengers.findog.src.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -17,11 +15,5 @@ public class MypageRepository {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    // 회원정보 변경
-    public int updateUserInfo(User user) {
-        String query = "update User set nickname = ?, password = ?, phoneNum = ? where userId = ? "; // 해당 userIdx를 만족하는 User를 해당 nickname으로 변경한다.
-        Object[] modifyUserParams = new Object[]{user.getNickname(), user.getPassword(), user.getPhoneNum(), user.getUserId()};
 
-        return this.jdbcTemplate.update(query, modifyUserParams); // 대응시켜 매핑시켜 쿼리 요청(생성했으면 1, 실패했으면 0)
-    }
 }
