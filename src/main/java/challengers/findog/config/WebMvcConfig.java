a7d212755993966a0080a343df98675665b52add
nayooniverse <kangna99@naver.com>
2022-05-23 21:54:17 +0900

    [fix] cors 수정

--- src/main/java/challengers/findog/config/WebMvcConfig.java
+++ src/main/java/challengers/findog/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3001", "http://localhost:3000", "http://www.findog.co.kr")
+                .allowedOrigins("http://localhost:3001", "http://localhost:3000", "https://findog.co.kr")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
