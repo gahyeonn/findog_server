6de73377aa444073df048264eecbac2ab4b8101e
GaHyeon <parka0017@gmail.com>
2022-05-24 13:52:54 +0900

    [fix] cors 변경 (#98)
    * [fix] cors 변경
    
    * [fix] cors 변경

--- src/main/java/challengers/findog/config/WebMvcConfig.java
+++ src/main/java/challengers/findog/config/WebMvcConfig.java
@@ -11,15 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://localhost:3001")
-                .allowedOrigins("https://findog.co.kr")
-                .allowedOrigins("https://findog.co.kr:3000")
-                .allowedOrigins("https://findog.co.kr:3001")
-                .allowedOrigins("https://main--celebrated-sawine-8ea9c1.netlify.app")
-                .allowedOrigins("https://main--celebrated-sawine-8ea9c1.netlify.app:3000")
-                .allowedOrigins("https://main--celebrated-sawine-8ea9c1.netlify.app:3001")
-                .allowedOrigins("")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
