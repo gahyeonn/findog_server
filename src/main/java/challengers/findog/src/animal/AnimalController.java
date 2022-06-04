ad3d4289727823d711c83bf8abfc6bc222b98454
GaHyeon <parka0017@gmail.com>
2022-06-04 14:15:17 +0900

    [feat] 유기동물 공고 사진 검색 기능 (#135)
    * [feat] 유기동물 공고 자동 업데이트 기능 구현
    
    * [fix] 유기동물 공조 조회 response 변경 (#125)
    
    * [fix] 게시글 삭제 시 댓글 존재 여부 확인 추가 (#128)
    
    * [fix] 게시글 작성 지역 추가
    
    * [fix] 날짜 포맷 변경 (#132)
    
    * [feat] 유기동물 공고 사진 검색 기능 구현 (#134)
    
    Co-authored-by: nayooniverse <kangna99@naver.com>
    Co-authored-by: nayooniverse <65649518+kangna99@users.noreply.github.com>

--- src/main/java/challengers/findog/src/animal/AnimalController.java
+++ src/main/java/challengers/findog/src/animal/AnimalController.java
@@ -2,15 +2,29 @@ package challengers.findog.src.animal;
 
 import challengers.findog.config.BaseException;
 import challengers.findog.config.BaseResponse;
-import challengers.findog.src.animal.model.Animal;
-import challengers.findog.src.animal.model.DeleteUnlikeAnimalReq;
-import challengers.findog.src.animal.model.GetAnimalListRes;
-import challengers.findog.src.animal.model.PageCriteriaDto;
+import challengers.findog.src.animal.model.*;
 import challengers.findog.utils.JwtService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.shaded.json.JSONObject;
+import com.nimbusds.jose.shaded.json.parser.JSONParser;
+import com.nimbusds.jose.shaded.json.parser.ParseException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedReader;
+import java.io.DataInput;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +34,16 @@ import java.util.List;
 public class AnimalController {
     private final AnimalService animalService;
     private final JwtService jwtService;
+    private static final RestTemplate REST_TEMPLATE;
+
+    static {
+        // RestTemplate 기본 설정을 위한 Factory 생성
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(3000);
+        factory.setReadTimeout(3000);
+        factory.setBufferRequestBody(false); // 파일 전송은 이 설정을 꼭 해주자.
+        REST_TEMPLATE = new RestTemplate(factory);
+    }
 
     /**
      * 유기동물 리스트 조회 API
@@ -132,4 +156,55 @@ public class AnimalController {
 
     }
 
+    /**
+     * 유기동물 공고 사진 검색
+     * @param getImageReq
+     * @return
+     */
+    @PostMapping("/searchImage")
+    public ResponseEntity<?> searchImage(@ModelAttribute GetImageReq getImageReq) throws IOException {
+    //public BaseResponse<GetAnimalListRes> uploadImages(@ModelAttribute GetImageReq getImageReq) {
+
+        LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        JsonNode response;
+        HttpStatus httpStatus = HttpStatus.CREATED;
+
+        try {
+            if (!getImageReq.getInput().isEmpty()) {
+                map.add("input", getImageReq.getInput().getResource());
+            }
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+            HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity
+                    = new HttpEntity<>(map, headers);
+            response = REST_TEMPLATE.postForObject(getImageReq.getServerUrl(), requestEntity, JsonNode.class);
+
+        } catch (HttpStatusCodeException e) {
+            HttpStatus errorHttpStatus = HttpStatus.valueOf(e.getStatusCode().value());
+            String errorResponse = e.getResponseBodyAsString();
+            return new ResponseEntity<>(errorResponse, errorHttpStatus);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+//        }  catch (BaseException e){
+//            return new BaseResponse<>(e.getStatus());
+//        }
+        //return new ResponseEntity<>(response, httpStatus);
+        //return new BaseResponse<>(searchAnimals("breed" = response.toString()));
+
+        String classNm = null;
+        try{
+            JSONParser parser	= new JSONParser();
+            JSONObject obj 		= (JSONObject)parser.parse(response.toString());
+            classNm = obj.get("class_name").toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        BaseResponse<GetAnimalListRes> getAnimalListRes = searchAnimals(1, 6, null, null, null, classNm, null);
+
+        return new ResponseEntity<>(getAnimalListRes, httpStatus);
+    }
 }
